e27b432ffc3033f703ee071679e492bbca0a2a5e
Raham009 <85884787+Raham009@users.noreply.github.com>
2023-02-01 17:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,6 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-#
-#  backend "remote" {
-#    organization = "your-organisation-name-here"
-#
-#    workspaces {
-#      name = "your-terraform-workspaces-name-here
-#    }
-#  }
 }
 provider "aws" {
   region = "us-east-1"
@@ -21,12 +13,5 @@ resource "aws_instance" "linuxserver" {
   instance_type = "t2.micro"
   tags = {
     Name = "poc-github-actions"
-  }
-}
-resource "aws_ebs_volume" "linuxvolume" {
-  availability_zone = "us-east-1a"
-  size              = 30
-  tags = {
-    Name = "LinuxVolume"
   }
 }
