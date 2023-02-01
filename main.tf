2b865f98e3999a39102f9305db72212eebe5c64a
Raham009 <rahams.009@gmail.com>
2023-02-01 19:15:46 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "linuxserver" {
-  ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "poc-github-actions"
-  }
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 }
--- provider.tf
+++ provider.tf
@@ -1,14 +1,12 @@
+# Configure the AWS Provider
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.76.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region  = var.region
-  profile = var.aws_profile
-
 }
