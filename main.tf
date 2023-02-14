4443e4b683e06e31a606e779da2c19fee3b88226
Raham009 <rahams.009@gmail.com>
2023-02-14 16:17:03 +0530

    First commit to execute powershell script

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "app" {
   ami           = var.ami_id
   instance_type = var.instance_type
 }
+resource "null_resource" "PowerShellScript" {
+  provisioner "local-exec" {
+    command = ".\script.ps1"
+    interpreter = ["pwsh", "-Command"]
+  }
+}
