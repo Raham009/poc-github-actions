60b1119623f22f2b0a8560df78cbc36abf2bfc09
Raham009 <rahams.009@gmail.com>
2023-02-27 17:34:56 +0530

    Adding source code workflow to the main branch

---
+++ test.py
@@ -0,0 +1,32 @@
+# Python 3 program to find
+# factorial of given number
+def factorial(n):
+	
+	# single line to find factorial
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);
+
+# Driver Code
+num = 5;
+print("Factorial of",num,"is",
+factorial(num))
+
+username = "xyz"
+password ="dgna@1788BNK"
+
+api_key = "aKNBBYI76789"
+token = "AJNBT8799FVH"
+
+
+import os
+os.system('dir')
+
+input_location = 'C:\\Users\Raham009\sample.txt'
+
+os.path.split(input_location) == True
+print('Testing for LFI')
+
+pip install dnspython:1.16.0
+
+def avg(marks):
+    assert len(marks) != 0 "List is blank"
+    return sum(marks)/len(marks)
